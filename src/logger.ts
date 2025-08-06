94b634f31eea7f4d8eb4342da398262a2f739ba7
Щемелёв Евгений Владимирович <ESchemelev@alfabank.ru>
2025-08-06 14:28:54 +0300

    feat(logger): write logs to a file

--- src/logger.ts
+++ src/logger.ts
@@ -1,12 +1,26 @@
+import path from "path";
 import pino from "pino";
 
-export const logger = pino({
-    transport: {
-        target: "pino-pretty",
-        options: {
-            colorize: true,
-            translateTime: "yyyy-mm-dd HH:MM:ss",
-            ignore: "pid,hostname",
+const transport = pino.transport({
+    targets: [
+        {
+            target: "pino-pretty",
+            options: {
+                colorize: true,
+                translateTime: "yyyy-mm-dd HH:MM:ss",
+                ignore: "pid,hostname",
+            },
         },
-    },
+        {
+            target: "pino-pretty",
+            options: {
+                colorize: false,
+                destination: path.resolve(__dirname, "../app.log"),
+                translateTime: "yyyy-mm-dd HH:MM:ss",
+                ignore: "pid,hostname",
+            },
+        },
+    ],
 });
+
+export const logger = pino({}, transport);
